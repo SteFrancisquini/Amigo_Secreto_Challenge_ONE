426f962c61188f6613817876266e0ff3de0fd2a9
Ste Francisquini <stephany.francisquini@proton.me>
2025-03-11 22:13:59 -0300

    🛠️  Atualiza README e  incluí código do backend

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Amigo_Secreto_Challenge_ONE
+# Challenge - Amigo Secreto (ONE) 🫂   
+O ONE é um programa de educação e empregabilidade com objetivo social de capacitar pessoas em tecnologia e conectá-las com o mercado de trabalho por meio de empresas parceiras.
+
+O projeto desenvolvido nesta etapa foi um sorteador de amigo secreto, uma aplicação em JavaScript que permite ao usuário adicionar e sortear nomes de uma forma intuitiva.
+
+## Funcionalidades 
+* Adicionar participantes
+* Visualização da lista de participantes
+* Embaralhamento e sorteio de nomes
+* Validação que impede campo vazio
+* Funções:  
+            \- Limpar lista de participantes\
+            - Realizar um novo sorteio\
+            - Garantir que o usuário não tire seu próprio nome
+
+## Visualizando o projeto 
+<img src="https://EM_BREVE.png"  />
+
+## Tecnologias Utilizadas
+
+   | Tecnologia  | Descrição|
+| ------------- | ------------- |
+| HTML  | Estrutura do conteúdo web |
+| CSS | Estilizar e formatar a aparência de páginas web | 
+| JavaScript | Adicionar interatividade e dinamismo a páginas web |
+
+##   Conceitos Aplicados 
+
+    Estruturas e manipulação de dados (Arrays)
+    Funções 
+    Manipulação do DOM (Document Object Model)
+    Variáveis (const/let)
+    Controle de fluxo (if, else if, else)
+    Estruturas de repetição (for)
+    
--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let amigosSorteados = [];
+let nome = '';
+
+function adicionarAmigo() {
+  const amigo = document.querySelector('#amigo');
+
+  if (amigo.value === '') {
+    alert('Campo vazio! Por favor, insira um nome.')
+  } else {
+    amigos.push(amigo.value);
+    const listaAmigos = document.querySelector('#listaAmigos')
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+      listaAmigos.innerHTML += `<li>${amigos[i]}</li>`
+    }
+    amigo.value = '';
+  }
+};
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert('Adicione um amigo primeiro!');
+  } else {
+    const numeroSorteado = Math.floor(Math.random() * (amigos.length - 0) + 0);
+    if (nome === amigos[numeroSorteado]) {
+      alert('Você mesmo foi sorteado, tente novamente!');
+      return;
+    }
+    if (amigosSorteados.indexOf(amigos[numeroSorteado]) < 0) {
+      const resultado = document.querySelector('#resultado');
+      resultado.innerHTML = amigos[numeroSorteado];
+      amigosSorteados.push(amigos[numeroSorteado]);
+
+    } else {
+      if (amigosSorteados.length === amigos.length) {
+        alert('Todos amigos já foram sorteados, inicie o jogo novamente!');
+      } else {
+        sortearAmigo();
+      }
+
+    }
+  }
+};
+
+function salvarNome() {
+  const nomePessoa = document.querySelector('#nome')
+  nome = nomePessoa.value;
+}
+
+function novoSorteio() {
+  nome = '';
+  amigos = [];
+  amigosSorteados = [];
+  document.querySelector('#nome').value = '';
+  document.querySelector('#listaAmigos').innerHTML = '';
+  document.querySelector('#resultado').innerHTML = '';
+
+}
+
+
